55e6b4d2dd59c323a564d1c1da3e87cef90f754d
Nicolas Cusson <ncusson@localhost.localdomain>
2015-06-12 14:00:33 -0400

    Added leaflet map in map tab

--- www/app/indicator/indicator.controller.js
+++ www/app/indicator/indicator.controller.js
@@ -1,13 +1,31 @@
-angular.module('gisMobile').controller('IndicatorCtrl',  function($scope, $state, $ionicNavBarDelegate, Indicator){
+angular.module('gisMobile').controller('IndicatorCtrl',  function($scope, $state, $ionicNavBarDelegate, Indicator, $ionicSideMenuDelegate, $ionicPlatform){
     var tabTitles = {
         map : 'Carte',
         graph : 'Graphique',
         table : 'Tableau'
     }
+    var map;
 
     $scope.setTab = function(tab){
         $scope.tab = tab;
         $ionicNavBarDelegate.title(Indicator.getIndicator($state.params.id).name + " - " + tabTitles[tab]);
+    }
+
+    $scope.init = {
+        map: function(){
+            console.log('Initializing map');
+            //Load leaflet
+            map = L.map('gis-map', { zoomControl:false }).setView([45.88451167585413, -72.50152587890625], 10);
+            // add an OpenStreetMap tile layer
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+            map.attributionControl.setPrefix('');
+        },
+        graph: function(){
+            console.log('Initializing graph');
+        },
+        table: function(){
+            console.log('Initializing table');
+        }
     }
 
     $scope.setTab('map');
