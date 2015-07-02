ed40d1f70a525ec8d0a69ca20a8e634a0771240b
Nicolas Cusson <ncusson@localhost.localdomain>
2015-07-02 15:20:56 -0400

    Implemented geometry service

--- www/app/services/geometry.service.js
+++ www/app/services/geometry.service.js
@@ -1,11 +1,92 @@
-angular.module('gisMobile').service('Geometry', function(localStorage, xmlparser){
+angular.module('gisMobile').service('Geometry', function(localStorage, xmlparser, STRUCTURE_URL, STRUCTURE_JSON, GEOMETRY_JSON){
     //Source proj
     proj4.defs("EPSG:32188","+proj=tmerc +lat_0=0 +lon_0=-73.5 +k=0.9999 +x_0=304800 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
     //Destination proj
     proj4.defs("EPSG:4326", "+proj=longlat +datum=WGS84 +no_defs ");
 
     //Load geometry
-    //Get geometry
+    function load(){
+        //Hook posList call back to GEOMETRY_JSON
+        GEOMETRY_JSON.domainSet.MultiSurface.surfaceMembers.Polygon.exterior.posList.callback = processPosList;
+        GEOMETRY_JSON.domainSet.MultiSurface.surfaceMembers.Polygon.interior.posList.callback = processPosList;
+
+        return localStorage.getGeometry()
+        .then(function(geo){ //If we can find the geometry object, return it
+            return geo;
+        })
+        .catch(function(e){ //If not get the url from structure if it exists
+            if(e.name != 'not_found') return e;
+            return localStorage.getStructure()
+            .catch(function(){ //If structure doesnt exist load it
+                if(e.name != 'not_found') return e;
+
+                return xmlparser.readFile(STRUCTURE_URL, STRUCTURE_JSON)
+                .then(function(structure){
+                    localStorage.saveStructure(structure);
+                    return structure.geometry;
+                });
+            }).then(function(geometry){ // Finally, get geometry from url
+                return xmlparser.readFile(geometry.url, GEOMETRY_JSON)
+                .then(function(geo){
+                    localStorage.saveGeometry(geo);
+                    return geo;
+                });
+            });
+        });
+    }
+
     //Change projection
+    function changeProjection(point){
+        return proj4('EPSG:32188','EPSG:4326', point);
+    }
+
+    //Convert into leaflet poin
+    function toLeafletPoint(point){
+        return new L.LatLng(point[1], point[0]);
+    }
+
+    //Process posList (Split it, change the projection, convert to leaflet point)
+    function processPosList(posListNode){
+        var rawPos = posListNode.content.trim().split(' ');
+        var result = [];
+        for (var i = rawPos.length - 1; i >= 0; i--) {
+            result.push(toLeafletPoint(changeProjection(rawPos[i].split(','))));
+        };
+        // console.log(result);
+        posListNode.content = result;
+        return result;
+    }
+
+    //Get geometry
+    function get(){
+        return load()
+        .then(function(geo){
+            return geo;
+        });
+    }
+
     //Validate version
+    function validate(){
+        var localVersion;
+        var structVersion;
+        return localStorage
+        .getGeometry()
+        .then(function(geo){
+            localVersion = geo.version.content;
+            return localStorage.getStructure()
+            .catch(function(e){
+                return xmlparser.readFile(STRUCTURE_URL, STRUCTURE_JSON);
+            })
+            .then(function(structure){
+                structVersion = structure.geometry.version;
+                return localVersion == structVersion;
+            });
+        })
+        .catch(function(e){ return false; });
+    }
+
+    return {
+        get: get,
+        validate : validate
+    }
 });
--- www/app/services/geometry.spec.js
+++ www/app/services/geometry.spec.js
@@ -1,16 +1,116 @@
 describe('Geometry service', function(){
-    var $q, $rootScope, Geometry;
+    var $q, $rootScope, Geometry, localStorageMock, xmlParserMock;
+    var readFileSpy;
+    
+    beforeEach(module('gisMobile', function($provide){
+        xmlParserMock = {
+            readFile: function(url, json){
+                var defer = $q.defer();
+                if(url == '/some/place/geometry.xml')
+                    defer.resolve(geoMock);
+                else
+                    defer.resolve(structMock);
+                return defer.promise;
+            }
+        };
+        localStorageMock = {
+            isGeometryCached: false,
+            isStructureCached: false,
+            getStructure: function(){
+                var defer = $q.defer();
+                if(localStorageMock.isStructureCached)
+                    defer.resolve(structMock);
+                else
+                    defer.reject({name: 'not_found'});
+                return defer.promise;
+            },
+            getGeometry: function(){
+                var defer = $q.defer();
+                if(localStorageMock.isGeometryCached){
+                    defer.resolve(geoMock);
+                }
+                else
+                    defer.reject({name: 'not_found'});
+                return defer.promise;
+            },
+            saveGeometry: function(){
+                localStorageMock.isGeometryCached = true;
+            },
+            saveStructure: function(){
+                localStorageMock.isStructureCached = true;
+            }
+        };
+
+        $provide.value('xmlparser', xmlParserMock);
+        $provide.value('localStorage', localStorageMock);
+
+        readFileSpy = spyOn(xmlParserMock, 'readFile').and.callThrough();
+        spyOn(localStorageMock, 'getStructure').and.callThrough();
+        spyOn(localStorageMock, 'getGeometry').and.callThrough();
+        spyOn(localStorageMock, 'saveGeometry').and.callThrough();
+    }));
+    
     beforeEach(inject(function(_$q_,_$rootScope_,_Geometry_){
         $q = _$q_;
         $rootScope = _$rootScope_;
         Geometry = _Geometry_;
     }));
-    beforeEach(module('gisMobile', function($provide){
-        xmlParserMock;
-        localStorageMock;
-    }));
-    it('should load geometry from file');
-    it('should load geometry from cache');
-    it('should be able to change projection');
-    it('should validate the cached version against the structure document');
+
+    it('should load geometry from file', function(done){
+        Geometry.get()
+        .then(function(geo){
+            done();
+        });
+        $rootScope.$digest();
+        expect(xmlParserMock.readFile).toHaveBeenCalled();
+        expect(localStorageMock.saveGeometry).toHaveBeenCalled();
+    });
+
+    it('should load geometry from cache', function(done){
+        localStorageMock.isGeometryCached = true;
+        localStorageMock.isStructureCached = true;
+        Geometry.get()
+        .then(function(geo){
+            done();
+        });
+        $rootScope.$digest();
+        expect(readFileSpy.calls.count()).toBe(0);
+    });
+
+    // it('should be able to change projection');
+    it('should validate the cached version against the structure document', function(done){
+        localStorageMock.isGeometryCached = true;
+        localStorageMock.isStructureCached = true;
+        Geometry.validate()
+        .then(function(isValid){
+            expect(isValid).toBe(true);
+            done();
+        });
+        $rootScope.$digest();
+    });
+
+    var geoMock = {
+        zone: [{
+            id: 'z0',
+            name: 'First zone',
+            description: 'Something',
+            Polygon: {
+                exterior: { posList: '20 30, 30 40, 40 20, ...' },
+                interior: [{ posList: '...' }, { posList: '...' }]
+            }
+        }],
+        version: {
+            date: '20/10/2012',
+            content: '1.0'
+        }
+    };
+
+    var structMock = {
+        geometry: {
+            name: "cdq",
+            label: "Centre-du-Québec",
+            version: "1.0",
+            url: "/some/place/geometry.xml"
+        }
+    };
 });
