91cddc6da1340122e82bd3e6181b16b7a92afcdf
Nicolas Cusson <ncusson@localhost.localdomain>
2015-07-02 10:06:32 -0400

    Implemented missing functions

--- www/app/services/indicator.service.js
+++ www/app/services/indicator.service.js
@@ -1,103 +1,171 @@
-angular.module('gisMobile').service('Indicator', function(xmlparser, $q){
+angular.module('gisMobile').service('Indicator', function(xmlparser, $q, localStorage, $rootScope){
     var indicators = [];
-    var categoriesDoc = false;
     var apiUrl = '/lib/xmlDocuments/indicator.xml';
 
     //Load the categories
     function loadCategories(){
-        var structure = {
-            category : {
-                attrs: ['name', 'label'],
-                indicator: {
-                    name: { attrs: ['$content'] },
-                    label: { attrs: ['$content'] },
-                    version: { attrs: ['$content'] },
-                    url: { attrs: ['$content'] }
-                }
-            }
-        }
-        return $q(function(resolve, reject){
-            if(categoriesDoc) return resolve(categoriesDoc.category);
-            xmlparser.readFile(apiUrl, structure)
+        //Todo check for connection before loading from local storage  
+        return localStorage.getStructure()
+        .then(function(struct){
+            return struct.category;
+        })
+        .catch(function(e){
+            if(e.name != 'not_found') return e;
+            
+            return xmlparser.readFile(apiUrl, structureStruct)
             .then(function(doc){
-                categoriesDoc = doc;
-                resolve(doc.category)
-            })
-            .catch(function(e){
-                reject(e);
+                localStorage.saveStructure(doc);
+                return doc.category;
             });
         });
     }
 
     //Get categories 
     function getCategories(){
-        return $q(function(resolve, reject){
-            loadCategories()
-            .then(function(cats){
-                categories = [];
-                for (var i = cats.length - 1; i >= 0; i--) {
-                    categories.push({
-                        name: cats[i].label,
-                        icon: 'ion-map',
-                        link: '/cat/indicators/' + cats[i].name,
-                        url: cats[i].url
-                    });
-                };
-                resolve(categories);
-            })
-            .catch(function(e){
-                reject(e);
-            });
+        return loadCategories()
+        .then(function(cats){
+            categories = [];
+            for (var i = cats.length - 1; i >= 0; i--) {
+                categories.push({
+                    name: cats[i].label,
+                    icon: 'ion-map',
+                    link: '/cat/indicators/' + cats[i].name,
+                });
+            };
+            return categories;
         });
     }
 
     //Get indicators by category
     function getIndicators(catName){
-        var deferred = $q.defer();
-
-        loadCategories().then(function(cats){
+        return loadCategories().then(function(cats){
             var cat = _.find(cats, { 'name': catName });
-            if(cat) return deferred.resolve(cat.indicator);
-            deferred.reject(catName + " not found");
-        })
-        .catch(function(e){
-            deferred.reject(e);
+            if(cat) return cat.indicator;
+            return $q.reject(catName + " not found");
         });
-
-        return deferred.promise;
     }
 
     //Get indicator by name
-    function getIndicator(name){
-        var deferred = $q.defer();
+    function getIndicatorSummary(name){
+        return loadCategories().then(function(cats){
+            var indicator = _.find( _.flatten( _.pluck(cats, 'indicator') ), function(ind){ return ind.name.content == name });
 
-        loadCategories().then(function(cats){
-            var indicator = _.find(_.flatten(_.pluck(cats, 'indicator')), function(ind){ return ind.name.content == name });
+            if(!indicator) return $q.reject(name + ' not found');
+            
             var result = {};
-            if(!indicator) return deferred.reject(name + ' not found');
             
             _.forOwn(indicator, function(value, key){
                 result[key] = value.content;
             });
 
-            deferred.resolve(result);
-        })
-        .catch(function(e){
-            deferred.reject(e);
+            return result;
         });
-
-        return deferred.promise;
     }
 
-    //Validate indicator version with localstorage
+    //Check if indicator version is the same as localStorage
+    function validateIndicator(name){
+        var localVersion;
+        var listVersion;
+        return localStorage.getIndicator(name)
+        .then(function(local){
+            localVersion = local.version.content;
+            return getIndicatorSummary(name)
+        })
+        .then(function(summary){
+            listVersion = summary.version;
+            return localVersion == listVersion;
+        })
+        .catch(function(e){
+            return false;
+        });
+    }
 
     //Load indicator details
+    function getIndicator(name){
+        return localStorage
+        .getIndicator(name)
+        .catch(function(e){
+            //Indicator was not found, fetch it from http
+            var xmlFile;
+            var staticPart;
+            var summary;
+            return getIndicatorSummary(name).then(function(sum){
+                summary = sum;
+                return xmlparser.loadFile(summary.url);
+            })
+            .then(function(fileContent){
+                //Parse the static part of the file
+                xmlFile = fileContent;
+                return xmlparser.readXML(xmlFile, indicatorStaticStruct);
+            })
+            .then(function(staticP){
+                //Parse the dynamic part of the file
+                staticPart = staticP;
+                var dynamicStruct = getDynamicStructure(staticPart);
+                return xmlparser.readXML(xmlFile, dynamicStruct);
+            })
+            .then(function(dynamicP){
+                //Merge the 3 parts
+                staticPart.value = dynamicP.value;
+                staticPart.name = summary.name;
+                staticPart.label = summary.label;
+                staticPart.url = summary.url;
+                staticPart.description = staticPart.description.content;
 
+                //Save it
+                localStorage.saveIndicator(staticPart);
+
+                return staticPart;
+            });
+        });
+    }
+
+    //Structure of the xml documents
+    var structureStruct = {
+        category : {
+            attrs: ['name', 'label'],
+            indicator: {
+                attrs: ['$isArray'],
+                name: { attrs: ['$content'] },
+                label: { attrs: ['$content'] },
+                version: { attrs: ['$content'] },
+                url: { attrs: ['$content'] }
+            }
+        }
+    }
+
+    var indicatorStaticStruct = {
+        name: { attrs: ['$content'] },
+        description: { attrs: ['$content'] },
+        param: {attrs: ['name', 'type', '$content'] },
+        legend: {
+            attrs: ['value', 'for'],
+            item: { attrs: ['min','max','color','$content'] }
+        },
+        version: { attrs: ['date', '$content'] }
+    }
+
+    //Build the dynamic structure for extration
+    function getDynamicStructure(staticPart){
+        var params = staticPart.param;
+        var dynamicStruct = {
+            value: { attrs: ['z'] },
+        };
+
+        for (var i = params.length - 1; i >= 0; i--) {
+            dynamicStruct.value.attrs.push(params[i].name)
+        };
+
+        return dynamicStruct;
+    }
+
+    //Return the public api
     return {
         indicators: indicators,
         getCategories: getCategories,
         getIndicator: getIndicator,
+        getIndicatorSummary: getIndicatorSummary,
         getIndicators: getIndicators,
-        tryLoadCats: loadCategories
+        validateIndicator: validateIndicator
     }
 });
--- www/app/services/indicator.spec.js
+++ www/app/services/indicator.spec.js
@@ -1,34 +1,98 @@
 
 describe("Indicator service", function() {
     var basePathXml = '/base/lib/xmlDocuments/';
-    var Indicator, $httpBackend, $rootScope;
+    var Indicator, $httpBackend, $rootScope, $pouchdb, $q, $injector;
+    var xmlParserMock, localStorageMock;
+    var readFileSpy, getStructureSpy, saveStructureSpy;
 
-    beforeEach(module('gisMobile'));
-    beforeEach(inject(function($injector) {
-        $httpBackend = $injector.get('$httpBackend');
-        Indicator = $injector.get('Indicator');
-        $rootScope = $injector.get('$rootScope');
+    //Mock gis-mobile
+    beforeEach(module('gisMobile', function($provide){
+        xmlParserMock = {
+            readFileCount: 0,
+            readFile: function(url){
+                var defer = $q.defer();
+                defer.resolve(indicatorJsonMock);
+                return defer.promise;
+            },
+            loadFile: function(url){
+                var defer = $q.defer();
+                defer.resolve('mock');
+                return defer.promise;
+            },
+            readXML: function(xml, json){
+                var defer = $q.defer();
+                switch(xmlParserMock.readFileCount){
+                    case 0:
+                        xmlParserMock.readFileCount ++;
+                        defer.resolve(sampleIndicatorStatic);
+                    break;
+                    case 1:
+                        xmlParserMock.readFileCount = 0;
+                        defer.resolve(sampleIndicatorValue);
+                    break;
+                }
+                return defer.promise;
+            }
+        };
 
-        $httpBackend.whenGET('/lib/xmlDocuments/indicator.xml')
-                    .respond(function(){ return [200, indicatorsXmlMock, {}] });
+        localStorageMock = {
+            isStructureCached: false,
+            isIndicatorCached: false,
+            getStructure: function(){
+                var defer = $q.defer();
+                if(localStorageMock.isStructureCached)
+                    defer.resolve(indicatorJsonMock);
+                else
+                    defer.reject({name: 'not_found'});
+                return defer.promise;
+            },
+            saveStructure: function(){
+                localStorageMock.isStructureCached = true;
+            },
+            getIndicator: function(){
+                var defer = $q.defer();
+                if(localStorageMock.isIndicatorCached)
+                    defer.resolve(sampleIndicator);
+                else
+                    defer.reject({name: 'not_found'});
+                return defer.promise;
+            },
+            saveIndicator: function(){
+                localStorageMock.isIndicatorCached = true;
+            }
+        }
+
+        $provide.value('xmlparser', xmlParserMock);
+        $provide.value('localStorage', localStorageMock);
+
+        //Enroll some spies
+        readFileSpy = spyOn(xmlParserMock, 'readFile').and.callThrough();
+        getStructureSpy = spyOn(localStorageMock, 'getStructure').and.callThrough();
+        saveStructureSpy = spyOn(localStorageMock, 'saveStructure').and.callThrough();
+    }));
+
+    beforeEach(inject(function(_$q_, _$rootScope_, _Indicator_) {
+        $q = _$q_;
+        $rootScope = _$rootScope_;
+        Indicator = _Indicator_;
     }));
 
     it('should list the categories', function(done){
-        $httpBackend.expectGET('/lib/xmlDocuments/indicator.xml');
-        Indicator.getCategories()
+        var promise = Indicator.getCategories()
         .then(function(categories){
-            expect(categories[0].name).toBe('Emplois par secteur d\'activité');
-            expect(categories[1].name).toBe('Emplois par niveau de compétence');
-            done()
+            expect(categories[1].name).toBe('Emplois par secteur d\'activité');
+            expect(categories[0].name).toBe('Emplois par niveau de compétence');
+            done();
         })
         .catch(function(e){
             expect(e).toBe(null);
         });
-        $httpBackend.flush();
+
+        $rootScope.$digest();
+        expect(readFileSpy).toHaveBeenCalled();
     });
 
     it('should list indicator by categories', function(done){
-        $httpBackend.expectGET('/lib/xmlDocuments/indicator.xml');
         var catName = 'activity_sector';
         Indicator.getIndicators(catName)
         .then(function(indicators){
@@ -37,90 +101,201 @@ describe("Indicator service", function() {
         .catch(function(e){
             expect(e).toBe(null);
         })
-        .finally(function(){ done() });
-        $httpBackend.flush();
+        .finally(done);
+        $rootScope.$digest();
     });
 
     it('should retrieve a single indicator', function(done){
-        $httpBackend.expectGET('/lib/xmlDocuments/indicator.xml');
-
-        Indicator.getIndicator('primary')
+        Indicator.getIndicatorSummary('primary')
         .then(function(indicator){ expect(indicator.name).toBe('primary'); })
         .catch(function(e){ expect(e).toBe(null); })
         .finally(function(){done()});
 
-        $httpBackend.flush();
+        $rootScope.$digest();
+        expect(readFileSpy).toHaveBeenCalled();
     });
 
     it('should retrieve a single indicator #2', function(done){
-        $httpBackend.expectGET('/lib/xmlDocuments/indicator.xml');
-
-        Indicator.getIndicator('something')
-        .then(function(indicator){ expect(indicator.name).toBe('something'); })
-        .catch(function(e){ expect(e).toBe(null); })
-        .finally(done);
-
-        $httpBackend.flush();
-    });
-
-    it('should fail when retrieving non existing indicator', function(done){
-        $httpBackend.expectGET('/lib/xmlDocuments/indicator.xml');
-
-        Indicator.getIndicator('unicorns')
-        .then(function(indicator){ expect(indicator.name).toBe(null); })
-        .catch(function(e){ expect(e).toBe('unicorns not found'); })
-        .finally(done);
-
-        $httpBackend.flush();
-    });
-
-    it('should cache the category document', function(done){
-        $httpBackend.expectGET('/lib/xmlDocuments/indicator.xml');
-
-        Indicator.getIndicator('something')
-        .then(function(indicator){  })
-        .catch(function(e){  })
-
-        $httpBackend.flush();
-
-        Indicator.getIndicator('something')
+        Indicator.getIndicatorSummary('something')
         .then(function(indicator){ expect(indicator.name).toBe('something'); })
         .catch(function(e){ expect(e).toBe(null); })
         .finally(done);
 
         $rootScope.$digest();
     });
+
+    it('should fail when retrieving non existing indicator', function(done){
+        Indicator.getIndicatorSummary('unicorns')
+        .then(function(indicator){ expect(indicator.name).toBe(null); })
+        .catch(function(e){ expect(e).toBe('unicorns not found'); })
+        .finally(done);
+
+        $rootScope.$digest();
+    });
+
+    it('should cache the category document', function(done){
+        Indicator.getIndicatorSummary('something')
+        .then(function(indicator){  })
+        .catch(function(e){  })
+
+        $rootScope.$digest();
+
+        Indicator.getIndicatorSummary('something')
+        .then(function(indicator){ expect(indicator.name).toBe('something'); })
+        .catch(function(e){ expect(e).toBe(null); })
+        .finally(done);
+
+        $rootScope.$digest();
+        expect(readFileSpy.calls.count()).toBe(1);
+    });
+
+    it('should load the details of the indicator', function(){
+        Indicator.getIndicator('primary')
+        .then(function(indicator){
+            expect(indicator.name).toBe('primary');
+            expect(indicator.param.length).toBe(2);
+            expect(indicator.value.length).toBe(2);
+        })
+        .catch(function(e){expect(e).toBe(null)});
+        $rootScope.$digest();
+    });
+
+    it('should validate an indicator version localStorage vs structure', function(done){
+        localStorageMock.isIndicatorCached = true;
+        Indicator.validateIndicator('primary')
+        .then(function(isValid){
+            expect(isValid).toBe(true);
+            done();
+        });
+        $rootScope.$digest();
+    });
+
+
+    //Mocked return objects
+    var indicatorJsonMock = {
+        category: [{
+            name: 'activity_sector',
+            label: "Emplois par secteur d'activité",
+            indicator: [{
+                name: { content: 'primary' },
+                label: { content: 'Primaire' },
+                version: { content: "1.0"  },
+                url: { content: "http://108.163.190.66/prototype/atlas_eq_2015/engine_data/emplois/2011/s_a_t_1_2011.xml" }
+            }, {
+                name: { content: 'secondary' },
+                label: { content: 'Secondaire' },
+                version: { content: "1.0" },
+                url: { content: "http://108.163.190.66/prototype/atlas_eq_2015/engine_data/emplois/2011/s_a_t_2_2011.xml" }
+            }]
+        },{
+            name: 'competency_level',
+            label: 'Emplois par niveau de compétence',
+            indicator: [{
+                name: { content: 'something' },
+                label: { content: 'Else' },
+                version: { content: "1.0" },
+                url: { content: "http://108.163.190.66/prototype/atlas_eq_2015/engine_data/emplois/2011/s_a_t_1_2011.xml" }
+            }]
+        }],
+        geometry: {
+            name: "cdq",
+            label: "Centre-du-Québec",
+            version: "1.0"
+        }
+    };
+
+    var sampleIndicatorSummary = {
+        name: 'something',
+        label: 'Else',
+        version: "1.0",
+        url: "http://108.163.190.66/prototype/atlas_eq_2015/engine_data/emplois/2011/s_a_t_1_2011.xml"
+    };
+
+
+    var sampleIndicatorStatic = {
+        name: 'primary',
+        description: 'This is a description',
+        param: [{
+            name: 'men',
+            type: 'Integer',
+            content: 'Number of men in the region'
+        },{
+            name: 'women',
+            type: 'Integer',
+            content: 'Number of women in the region'
+        }],
+        legend: [{
+            for: "map",
+            item: [{
+                min: '0',
+                max: '100',
+                color: 'yellow',
+                content: '0 à 100'
+            },{
+                min: '101',
+                max: '200',
+                color: 'red',
+                content: '101 à 200'
+            }]
+        }],
+        version: {
+            date: '10/20/2012',
+            content: '1.0'
+        }
+    };
+
+    var sampleIndicatorValue = {
+        value: [{
+            z: 'z0',
+            men: '100',
+            women: '200'
+        },{
+            z: 'z1',
+            men: '200',
+            women: '100'
+        }],
+    };
+
+    var sampleIndicator = {
+        name: 'primary',
+        description: 'This is a description',
+        label: 'Primaire',
+        url: "http://108.163.190.66/prototype/atlas_eq_2015/engine_data/emplois/2011/s_a_t_1_2011.xml",
+        param: [{
+            name: 'men',
+            type: 'Integer',
+            content: 'Number of men in the region'
+        },{
+            name: 'women',
+            type: 'Integer',
+            content: 'Number of women in the region'
+        }],
+        value: [{
+            z: 'z0',
+            men: '100',
+            women: '200'
+        },{
+            z: 'z1',
+            men: '200',
+            women: '100'
+        }],
+        legend: [{
+            for: "map",
+            item: [{
+                min: '0',
+                max: '100',
+                color: 'yellow',
+                content: '0 à 100'
+            },{
+                min: '101',
+                max: '200',
+                color: 'red',
+                content: '101 à 200'
+            }]
+        }],
+        version: {
+            date: '10/20/2012',
+            content: '1.0'
+        }
+    }
 });
-
-var indicatorsXmlMock = '<?xml version="1.0" ?>' +
-'<document>' +
-    '<category name="activity_sector" label="Emplois par secteur d\'activité">' +
-        '<indicator>' +
-            '<name>primary</name>' +
-            '<label>Primaire</label>' +
-            '<version>1.0</version>' +
-            '<url>/test/indicator_primary.xml</url>' +
-        '</indicator>' +
-        '<indicator>' +
-            '<name>secondary</name>' +
-            '<label>Secondaire</label>' +
-            '<version>1.0</version>' +
-            '<url>http://108.163.190.66/prototype/atlas_eq_2015/engine_data/emplois/2011/s_a_t_2_2011.xml</url>' +
-        '</indicator>' +
-    '</category>' +
-    '<category name="competency_level" label="Emplois par niveau de compétence">' +
-        '<indicator>' +
-            '<name>something</name>' +
-            '<label>Else</label>' +
-            '<version>1.0</version>' +
-            '<url>http://108.163.190.66/prototype/atlas_eq_2015/engine_data/emplois/2011/s_a_t_2_2011.xml</url>' +
-        '</indicator>' +
-    '</category>' +
-    '<geometry>' +
-        '<name>cdq</name>' +
-        '<label>Centre-du-Québec</label>' +
-        '<version>1.0</version>' +
-    '</geometry>' +
-'</document>';
-
-var sampleIndicator = '';
