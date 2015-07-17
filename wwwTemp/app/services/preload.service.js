3a8df76d09d69bc89c43fc8e674b062fe5a36e12
Nicolas Cusson <ncusson59@gmail.com>
2015-07-17 14:51:00 -0400

    Removed wwwTemp

--- wwwTemp/app/services/preload.service.js
+++
@@ -1,3 +0,0 @@
-angular.module('gisMobile').service('Preload', function(){
-    
-});
--- wwwTemp/app/settings/settings.js
+++
@@ -1,8 +0,0 @@
-angular.module('gisMobile').config(function($stateProvider){
-  $stateProvider
-  .state('settings', {
-    url: '/settings',
-    controller: 'SettingsCtrl',
-    templateUrl: 'app/settings/settings.html'
-  });
-});
