63fc62b13b768d7356c008b15bec331f161d5a0f
Nicolas Cusson <ncusson59@gmail.com>
2015-07-17 15:18:28 -0400

    Removed www

--- www/app/settings/settings.controller.js
+++
@@ -1,9 +0,0 @@
-angular.module('gisMobile').controller('SettingsCtrl', function($scope, localStorage){
-    $scope.settings = [{
-        name: 'Réinitialiser la mémoire',
-        icon: 'ion-android-sync',
-        action : function(){
-            localStorage.reset();
-        }
-    }]
-});
