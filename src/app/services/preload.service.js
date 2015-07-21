634570bda91b1ac908961d61f184a386c9049756
Nicolas Cusson <ncusson59@gmail.com>
2015-07-21 13:23:35 -0400

    Implemented preload service

--- src/app/services/preload.service.js
+++ src/app/services/preload.service.js
@@ -1,3 +1,115 @@
-angular.module('gisMobile').service('Preload', function(){
+angular.module('gisMobile').service('Preload', function($cordovaNetwork, $q, Indicator, Geometry, Alert, $cordovaSplashscreen){
+    var categories;
+    var Log = Logger.get('Preload');
     
+    function checkNetwork(){
+        return $q.when($cordovaNetwork.isOnline());
+    }
+
+    function reloadStructure(){
+        return Indicator.flushStructure()
+        .then(function(){
+            Log.info('Structure flushed');
+            return Indicator.getCategories();
+        }).then(function(){
+            Log.info('Structure cached');
+            return true;
+        })
+        .catch(function(e){
+            console.log(e);
+            return Indicator.getCategories();
+        });
+    }
+
+    function preloadGeometry(){
+        return Geometry.validate()
+        .then(function(isValid){
+            if(!isValid){
+                Log.info('Geometry invalid, reloading.');
+                return Geometry.flush().finally(function(){ 
+                    Log.info('Geometry flushed');
+                    return Geometry.get(); 
+                }).then(function(){
+                    Log.info('Geometry cached');
+                    return true;
+                });
+            }
+            Log.info('Geometry valid');
+            return true;
+        })
+        .catch(function(e){
+            Log.info('Geometry has been loaded from scratch');
+        })
+    }
+
+    function preloadAlerts(){
+        return Alert.list()
+        .then(function(alerts){
+            var indicators = [];
+            var promise;
+
+            _.forEach(alerts, function(a, index){
+                var ind = _.first(a.params);
+                indicators.push(ind);
+            });
+
+            _.forEach(_.uniq(indicators), function(id, index){
+                Indicator.validate(id)
+                .then(function(isValid){
+                    if(!isValid){
+                        Log.info(id + ' is not valid, reloading');
+                        promise = Indicator.flushIndicator(id)
+                        .then(function(){
+                            Log.info(id + ' has been flushed');
+                            return Indicator.get(id).then(function(){ 
+                                Log.info(id + ' has been reloaded');
+                            });
+                        })
+                        .catch(function(e){ 
+                            Log.info('Nothing to delete, trying to load');
+                            return Indicator.get(id).then(function(){ 
+                                Log.info(id + ' has been reloaded');
+                            });
+                        });
+                    }
+                    Log.info(id + ' is valid');
+                });
+            });
+
+            return promise;
+        });
+    }
+
+    function hideSplashScreen(){
+        return $q.when($cordovaSplashscreen.hide());
+    }
+
+    function go(){
+        return checkNetwork()
+        .then(function(isOnline){
+            if(isOnline){
+                return reloadStructure()
+                .then(preloadGeometry)
+                .then(preloadAlerts)
+                .then(hideSplashScreen);
+            }else{
+                return false;
+            }
+        })
+        .then(function(){
+            Log.info('Preloading complete.');
+        })
+        .catch(function(e){
+            console.log(e);
+            Log.info('An error occured during the preloading.');
+        })
+    }
+
+    return {
+        checkNetwork: checkNetwork,
+        reloadStructure: reloadStructure,
+        preloadGeometry: preloadGeometry,
+        preloadAlerts: preloadAlerts,
+        go: go
+    }
 });
--- src/app/services/preload.spec.js
+++ src/app/services/preload.spec.js
@@ -1,5 +1,26 @@
 describe('Preloading', function(){
-    it('should reload structure if network is availlable');
+    var Preload, $rootScope, MOCKS, $cordovaNetwork;
+    var localStorageMock;
+
+    beforeEach(module('gisMobile'));
+
+    beforeEach(inject(function(_$rootScope_, _Preload_, _MOCKS_, _$cordovaNetwork_){
+        Preload = _Preload_;
+        $rootScope = _$rootScope_;
+        MOCKS = _MOCKS_;
+        $cordovaNetwork = _$cordovaNetwork_;
+    }));
+
+    it('should check if network is availlable', function(done){
+        spyOn($cordovaNetwork, 'isOnline').and.callThrough();
+        Preload.checkNetwork()
+        .then(function(isOnline){
+            expect($cordovaNetwork.isOnline).toHaveBeenCalled();
+        })
+        .finally(done);
+        $rootScope.$digest();
+    });
+
     it('should verify that the geometry is valid');
     it('should verify that the indicator with alerts are valid');
     it('should reload invalid geometry');
