16e3d63435de2638a9d1b06820c4716177d1b388
Nicolas Cusson <ncusson59@gmail.com>
2015-08-11 09:58:38 -0400

    Implemented new structure service

--- src/app/services/structure.service.js
+++ src/app/services/structure.service.js
@@ -3,10 +3,91 @@ angular.module('gisMobile').service('Structure', function(xmlparser, $q, localSt
     var isStructureFresh;
 
     // Load structure from remote
+    function getRemote(){
+        return xmlparser.readFile(STRUCTURE_URL, STRUCTURE_JSON)
+        .then(function(structure){
+            Log.debug('Structure loaded from url');
+            localStorage.saveStructure(structure);
+            return structure;
+        });
+    }
+
     // Load structure from cache
+    function getLocal(){
+        return localStorage.getStructure()
+        .then(function(structure){
+            Log.debug('Structure loaded from cache');
+            return structure;
+        })
+        .catch(function(e){
+            Log.debug('Structure is not in localStorage');
+            return $q.reject(e);
+        });
+    }
+    
     // Get geometry
+    function getGeometry(){
+        return localStorage.getStructure()
+        .then(function(structure){
+            return structure.geometry;
+        });
+    }
+
     // Get categories
+    function getCategories(){
+        return localStorage.getStructure()
+        .then(function(structure){ 
+            return structure.category 
+        })
+    }
+    
     // Get indicator by categories
+    function getIndicatorFrom(catId){
+        return getCategories()
+        .then(function(cats){
+            var cat = _.find(cats, { id : catId });
+            if(cat){
+                return cat.indicator;
+            }else{
+                Log.debug('Category ' + catId + " was not found in localStorage");
+                return $q.reject({name : 'not_found'});
+            }
+        });
+    }
 
+    // Get offline indicators by categories
+    function getOfflineIndicatorFrom(catId){
+        return getIndicatorFrom(catId)
+        .then(function(indicators){
+            var promise;
+            var offlineIndicators = [];
+            
+            _.forEach(indicators, function(indicator){
+                promise = isIndicatorCached(indicator.id)
+                .then(function(isCached){
+                    if(isCached)
+                        offlineIndicators.push(indicator);
+                });
+            });
 
+            return promise.then(function(){
+                return offlineIndicators;
+            });
+        });
+    }
+
+    function isIndicatorCached(id){
+        return localStorage.getIndicator(id)
+        .then(function(ind){ return true; })
+        .catch(function(e){ return false; });
+    }
+
+    return {
+        getRemote : getRemote,
+        getLocal : getLocal,
+        getGeometry : getGeometry,
+        getCategories : getCategories,
+        getIndicatorFrom : getIndicatorFrom,
+        getOfflineIndicatorFrom : getOfflineIndicatorFrom
+    }
 });
--- src/app/services/structure.spec.js
+++ src/app/services/structure.spec.js
@@ -1,28 +1,111 @@
 describe('Structure', function(){
-    var Structure, $rootScope, MOCKS;
-    var localStorageMock;
+    var Structure, $rootScope;
+    var localStorageMock, xmlParserMock;
 
     beforeEach(function(){
         module('gisMobile');
         module('gisMobileMocks');
 
-        inject(function(_$rootScope_, _Structure_, _MOCKS_, _localStorage_){
+        inject(function(_$rootScope_, _Structure_, _MOCKS_, _localStorage_, _xmlparser_){
             Structure = _Structure_;
             $rootScope = _$rootScope_;
             MOCKS = _MOCKS_;
             localStorageMock = _localStorage_;
+            xmlParserMock = _xmlparser_;
+            
+            spyOn(xmlParserMock, 'readFile').and.callThrough();
+            spyOn(localStorageMock, 'getStructure').and.callThrough();
         });
     });
 
     it('should load from remote xml', function(done){
-        Structure.getFromRemote()
+        Structure.getRemote()
         .then(function(structure){
-
-        })
+            expect(structure.category.length).toBe(2);
+            expect(structure.geometry.id).toBe('cdq');
+            expect(xmlParserMock.readFile).toHaveBeenCalled();
+            done();
+        });
+        $rootScope.$digest();
     });
 
-    it('should load from localStorage');
-    it('should return categories');
-    it('should return geometry');
-    it('should return indicators by categories');
+    it('should load from localStorage', function(done){
+        localStorageMock.isStructureCached = true;
+        Structure.getLocal()
+        .then(function(structure){
+            expect(structure.category.length).toBe(2);
+            expect(structure.geometry.id).toBe('cdq');
+            expect(localStorageMock.getStructure).toHaveBeenCalled();
+            done();
+        });
+        $rootScope.$digest();
+    });
+
+    it('should tell when localStorage is not defined', function(){
+        Structure.getLocal()
+        .then(function(structure){ })
+        .catch(function(e){
+            expect(e.name).toBe('not_found');
+        });
+        $rootScope.$digest();
+    });
+
+    it('should return categories', function(done){
+        localStorageMock.isStructureCached = true;
+        Structure.getCategories()
+        .then(function(cats){
+            expect(cats.length).toBe(2);
+            expect(cats[0].label).toBe('Emplois par secteur d\'activité');
+            expect(cats[1].label).toBe('Emplois par niveau de compétence');
+            expect(localStorageMock.getStructure).toHaveBeenCalled();
+            done();
+        });
+        $rootScope.$digest();
+    });
+    
+    it('should return geometry', function(done){
+        localStorageMock.isStructureCached = true;
+        Structure.getGeometry()
+        .then(function(geo){
+            expect(geo.id).toBe('cdq');
+            expect(localStorageMock.getStructure).toHaveBeenCalled();
+            done();
+        });
+        $rootScope.$digest();
+    });
+
+    it('should return indicators by categories', function(done){
+        localStorageMock.isStructureCached = true;
+        Structure.getIndicatorFrom('activity_sector')
+        .then(function(indicators){
+            expect(indicators.length).toBe(2);
+            expect(indicators[0].id).toBe('primary');
+            done();
+        });
+        $rootScope.$digest();
+    });
+
+    it('should tell if specific categories id doent exist', function(done){
+        localStorageMock.isStructureCached = true;
+        Structure.getIndicatorFrom('rubish')
+        .then(function(indicators){ })
+        .catch(function(e){
+            expect(e.name).toBe('not_found');
+            done();
+        });
+        $rootScope.$digest();
+    });
+
+    it('should return offline indicators by categories', function(done){
+        localStorageMock.isStructureCached = true;
+        localStorageMock.isIndicatorCached = true;
+        Structure.getOfflineIndicatorFrom('activity_sector')
+        .then(function(indicators){
+            expect(indicators.length).toBe(1);
+            done();
+        });
+        $rootScope.$digest();
+    });
+
+    it('should be able to flush the structure');
 });
