cd9e2592ded276660bba14128e8d97143e5498c8
Nicolas Cusson <ncusson59@gmail.com>
2015-08-11 09:59:23 -0400

    Added logout and redirect to home

--- src/app/settings/settings.controller.js
+++ src/app/settings/settings.controller.js
@@ -1,9 +1,23 @@
-angular.module('gisMobile').controller('SettingsCtrl', function($scope, localStorage){
+angular.module('gisMobile').controller('SettingsCtrl', function($scope, localStorage, Auth, $rootScope, $state){
     $scope.settings = [{
         name: 'Réinitialiser la mémoire',
         icon: 'ion-android-sync',
         action : function(){
-            localStorage.reset();
+            localStorage.reset()
+            .then(function(){
+                $rootScope.isReloadForced = true;
+                $state.go('home');
+            });
+        }
+    }, {
+        name: 'Se déconnecter',
+        icon: 'ion-locked',
+        action : function(){
+            Auth.logout()
+            .then(function(){
+                $rootScope.isReloadForced = true;
+                $state.go('home');
+            });
         }
     }]
 });
