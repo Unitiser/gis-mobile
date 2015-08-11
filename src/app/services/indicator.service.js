7a3abd9edb625e56a1f69fe30d76bc67af16535b
Nicolas Cusson <ncusson59@gmail.com>
2015-08-11 09:56:10 -0400

    Split get local from get remote

--- src/app/services/indicator.service.js
+++ src/app/services/indicator.service.js
@@ -1,82 +1,67 @@
-angular.module('gisMobile').service('Indicator', function(xmlparser, $q, localStorage, $rootScope, STRUCTURE_URL, STRUCTURE_JSON, INDICATOR_JSON){
-    //Load the categories
-    function loadCategories(){
-        //Todo check for connection before loading from local storage  
-        return localStorage.getStructure()
-        .then(function(struct){
-            return struct.category;
-        })
-        .catch(function(e){
-            if(e.name != 'not_found') return e;
-            
-            return xmlparser.readFile(STRUCTURE_URL, STRUCTURE_JSON)
-            .then(function(doc){
-                localStorage.saveStructure(doc);
-                return doc.category;
-            });
-        });
-    }
-
-    //Get categories 
-    function getCategories(){
-        return loadCategories()
-        .then(function(cats){
-            categories = [];
-            for (var i = cats.length - 1; i >= 0; i--) {
-                categories.push({
-                    name: cats[i].label,
-                    icon: 'ion-map',
-                    link: '/cat/indicators/' + cats[i].id,
-                });
-            };
-            return categories;
-        });
-    }
-
-    //Get indicators by category
-    function getByCategory(catId){
-        return loadCategories().then(function(cats){
-            var cat = _.find(cats, { 'id': catId });
-            if(cat) return cat.indicator;
-            return $q.reject(catId + " not found");
-        });
-    }
-
-    //Get all cached indicators by category
-    function getOfflineByCategory(catId){
-        return getByCategory(catId).then(function(inds){
-            var offlineInds = [];
-            var promise;
-            _.forEach(inds, function(ind){
-                promise = isCached(ind.id).then(function(isCached){
-                    if(isCached) offlineInds.push(ind);
-                });
-            });
-            return promise.then(function(){ return offlineInds; });
-        });
-    }
-
+angular.module('gisMobile').service('Indicator', function(xmlparser, $q, localStorage, $rootScope, Structure, STRUCTURE_URL, STRUCTURE_JSON, INDICATOR_JSON){
     //Get indicator by id
     function getSummary(id){
-        return loadCategories().then(function(cats){
+        return Structure.getCategories()
+        .then(function(cats){
             var indicator = _.find( _.flatten( _.pluck(cats, 'indicator') ), function(ind){ return ind.id == id });
-
-            if(!indicator) return $q.reject(id + ' not found');
-
+            if(!indicator) { return $q.reject(id + ' not found'); }
             return indicator;
         });
+    }
+
+    // Load remote version of the indicator
+    function getRemote(id){
+        var xmlFile;
+        var staticPart;
+        var summary;
+
+        return getSummary(id)
+        .then(function(sum){
+            summary = sum;
+            return xmlparser.loadFile(summary.url);
+        })
+        .then(function(fileContent){
+            //Parse the static part of the file
+            xmlFile = fileContent;
+            return xmlparser.readXML(xmlFile, INDICATOR_JSON);
+        })
+        .then(function(staticP){
+            //Parse the dynamic part of the file
+            staticPart = staticP;
+            return xmlparser.readXML(xmlFile, getDynamicStructure(staticPart));
+        })
+        .then(function(dynamicPart){
+            //Merge the 3 parts
+            staticPart.value = dynamicPart.value;
+            staticPart.name = summary.id;
+            staticPart.label = summary.label;
+            staticPart.url = summary.url;
+            staticPart.description = staticPart.description.content;
+
+            //Save it
+            localStorage.saveIndicator(staticPart);
+            return staticPart;
+        });
+    }
+
+    // Try to load local version of the indicator.
+    function getLocal(id){
+        return localStorage.getIndicator(id);
     }
 
     //Check if indicator version is the same as localStorage
     function validate(id){
         var localVersion;
         var listVersion;
+        console.log(id);
         return localStorage.getIndicator(id)
         .then(function(local){
+            console.log(local);
             localVersion = local.version.content;
             return getSummary(id)
         })
         .then(function(summary){
+            console.log(summary);
             listVersion = summary.version;
             return localVersion == listVersion;
         })
@@ -92,44 +77,7 @@ angular.module('gisMobile').service('Indicator', function(xmlparser, $q, localSt
         .catch(function(e){ return false; });
     }
 
-    //Load indicator details
-    function get(id){
-        return localStorage
-        .getIndicator(id)
-        .catch(function(e){
-            //Indicator was not found, fetch it from http
-            var xmlFile;
-            var staticPart;
-            var summary;
-            return getSummary(id).then(function(sum){
-                summary = sum;
-                return xmlparser.loadFile(summary.url);
-            })
-            .then(function(fileContent){
-                //Parse the static part of the file
-                xmlFile = fileContent;
-                return xmlparser.readXML(xmlFile, INDICATOR_JSON);
-            })
-            .then(function(staticP){
-                //Parse the dynamic part of the file
-                staticPart = staticP;
-                var dynamicStruct = getDynamicStructure(staticPart);
-                return xmlparser.readXML(xmlFile, dynamicStruct);
-            })
-            .then(function(dynamicP){
-                //Merge the 3 parts
-                staticPart.value = dynamicP.value;
-                staticPart.name = summary.id;
-                staticPart.label = summary.label;
-                staticPart.url = summary.url;
-                staticPart.description = staticPart.description.content;
 
-                //Save it
-                localStorage.saveIndicator(staticPart);
-                return staticPart;
-            });
-        });
-    }
 
     //Build the dynamic indicator structure for extration
     function getDynamicStructure(staticPart){
@@ -154,13 +102,7 @@ angular.module('gisMobile').service('Indicator', function(xmlparser, $q, localSt
             dynamicStruct.value.attrs.push(params[i].name)
         };
 
-        console.log(dynamicStruct);
         return dynamicStruct;
-    }
-
-    //delete cached structure document
-    function flushStructure(){
-        return localStorage.flushStructure();
     }
 
     function flushIndicator(id){
@@ -169,13 +111,10 @@ angular.module('gisMobile').service('Indicator', function(xmlparser, $q, localSt
 
     //Return the public api
     return {
-        getCategories: getCategories,
-        get: get,
+        getRemote: getRemote,
+        getLocal: getLocal,
         getSummary: getSummary,
-        getByCategory: getByCategory,
-        getOfflineByCategory: getOfflineByCategory,
         validate: validate,
-        flushStructure: flushStructure,
         flushIndicator: flushIndicator,
         isCached: isCached
     }
--- src/app/services/indicator.spec.js
+++ src/app/services/indicator.spec.js
@@ -16,46 +16,20 @@ describe("Indicator service", function() {
             localStorageMock = _localStorage_;
         });
 
+        localStorageMock.isStructureCached = true;
         readFileSpy = spyOn(xmlParserMock, 'readFile').and.callThrough();
+        spyOn(xmlParserMock, 'loadFile').and.callThrough();
+        spyOn(xmlParserMock, 'readXML').and.callThrough();
         getStructureSpy = spyOn(localStorageMock, 'getStructure').and.callThrough();
         saveStructureSpy = spyOn(localStorageMock, 'saveStructure').and.callThrough();
         spyOn(localStorageMock, 'flushStructure').and.callThrough();
         spyOn(localStorageMock, 'flushIndicator').and.callThrough();
     });
 
-    it('should list the categories', function(done){
-        var promise = Indicator.getCategories()
-        .then(function(categories){
-            expect(categories[1].name).toBe('Emplois par secteur d\'activité');
-            expect(categories[0].name).toBe('Emplois par niveau de compétence');
-            expect(readFileSpy).toHaveBeenCalled();
-            done();
-        })
-        .catch(function(e){
-            expect(e).toBe(null);
-        });
-
-        $rootScope.$digest();
-    });
-
-    it('should list indicator by categories', function(done){
-        var catName = 'activity_sector';
-        Indicator.getByCategory(catName)
-        .then(function(indicators){
-            expect(indicators.length).toBe(2);
-        })
-        .catch(function(e){
-            expect(e).toBe(null);
-        })
-        .finally(done);
-        $rootScope.$digest();
-    });
-
     it('should retrieve a single indicator', function(done){
         Indicator.getSummary('primary')
         .then(function(indicator){ 
-            expect(indicator.id).toBe('primary'); 
-            expect(readFileSpy).toHaveBeenCalled();
+            expect(indicator.id).toBe('primary');
         })
         .catch(function(e){ expect(e).toBe(null); })
         .finally(function(){done()});
@@ -81,32 +55,30 @@ describe("Indicator service", function() {
         $rootScope.$digest();
     });
 
-    it('should cache the category document', function(done){
-        Indicator.getSummary('something')
-        .then(function(indicator){  })
-        .catch(function(e){  })
-
-        $rootScope.$digest();
-
-        Indicator.getSummary('something')
-        .then(function(indicator){ 
-            expect(indicator.id).toBe('something'); 
-            expect(readFileSpy.calls.count()).toBe(1);
-        })
-        .catch(function(e){ expect(e).toBe(null); })
-        .finally(done);
-
-        $rootScope.$digest();
-    });
-
-    it('should load the details of the indicator', function(){
-        Indicator.get('primary')
+    it('should load the details of a non cached indicator', function(done){
+        Indicator.getRemote('primary')
         .then(function(indicator){
             expect(indicator.name).toBe('primary');
             expect(indicator.param.length).toBe(2);
             expect(indicator.value.length).toBe(2);
+            expect(xmlParserMock.loadFile).toHaveBeenCalled();
+            expect(xmlParserMock.readXML).toHaveBeenCalled();
+            done()
         })
         .catch(function(e){expect(e).toBe(null)});
+
+        $rootScope.$digest();
+    });
+
+    it('should load details of a local indicator', function(done){
+        localStorageMock.isIndicatorCached = true;
+        Indicator.getLocal('primary')
+        .then(function(indicator){
+            expect(indicator.name).toBe('primary');
+            expect(indicator.param.length).toBe(2);
+            expect(indicator.value.length).toBe(2);
+            done();
+        });
         $rootScope.$digest();
     });
 
@@ -130,21 +102,6 @@ describe("Indicator service", function() {
         localStorageMock.isIndicatorCached = true;
         Indicator.isCached('primary')
         .then(function(isCached){ expect(isCached).toBe(true) }).finally(done);
-        $rootScope.$digest();
-    });
-
-    it('should load offline indicators by categories', function(done){
-        localStorageMock.isIndicatorCached = true;
-        Indicator.getOfflineByCategory('activity_sector')
-        .then(function(cats){ expect(cats.length).toBe(1); })
-        .finally(done);
-        $rootScope.$digest();
-    });
-
-    it('should be able to flush the structure', function(done){
-        Indicator.flushStructure()
-        .then(function(){ expect(localStorageMock.flushStructure).toHaveBeenCalled(); })
-        .finally(done);
         $rootScope.$digest();
     });
 
