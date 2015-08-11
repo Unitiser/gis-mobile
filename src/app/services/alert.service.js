eadad32d6591f24ae716317c95d0ec271f4ba1db
Nicolas Cusson <ncusson59@gmail.com>
2015-08-11 10:02:49 -0400

    Implemented Strucutre and Indicator changes, added toString

--- src/app/services/alert.service.js
+++ src/app/services/alert.service.js
@@ -1,4 +1,4 @@
-angular.module('gisMobile').service('Alert',function(Indicator, $q, localStorage){
+angular.module('gisMobile').service('Alert',function(Indicator, Structure, $q, localStorage){
 
     //List all alerts
     function list(){
@@ -12,7 +12,7 @@ angular.module('gisMobile').service('Alert',function(Indicator, $q, localStorage
 
     //Remove an alert
     function remove(alert){
-        return localStorage.removeAlert(alert.id);
+        return localStorage.removeAlert(alert._id);
     }
 
     //Functions used to extend Alert by type.
@@ -20,19 +20,44 @@ angular.module('gisMobile').service('Alert',function(Indicator, $q, localStorage
         zoneParam: function(comparator, indId, param, zone){ 
             var ctx = this;
             this.isThrown = function(){
-                return ctx.isDismissed ? $q.when(false) : Indicator.get(indId)
+                return ctx.isDismissed ? $q.when(false) : Indicator.getLocal(indId)
                         .then(function(ind){ 
                             return ind.version.content == ctx.resolved ? false : comparator(parseInt(_.find(ind.value, {z: zone})[param])); 
                         });
+            }
+
+            this.toString = function(){
+                var stringFormat = "La valeur de %s1 de %s5 de la zone %s6 %s2 %s3. La nouvelle valeur de %s1 est %s4.";
+                return Indicator.getLocal(indId)
+                .then(function(indicator){
+                    return stringFormat.replace(/%s1/g, param)
+                    .replace(/%s2/g, comparatorString[comparator.toString().split(' ')[0]])
+                    .replace(/%s3/g, comparator.toString().split(' ')[1])
+                    .replace(/%s4/g, _.find(indicator.value, {z: zone})[param])
+                    .replace(/%s5/g, indicator.label)
+                    .replace(/%s6/g, zone);
+                });
             }
         },
         totalParam: function(comparator, indId, param){
             var ctx = this;
             this.isThrown = function(){
-                return ctx.isDismissed ? $q.when(false) : Indicator.get(indId)
+                return ctx.isDismissed ? $q.when(false) : Indicator.getLocal(indId)
                         .then(function(ind){ 
                             return ind.version.content == ctx.resolved ? false : comparator(_.sum(ind.value, param)); 
                         });
+            }
+
+            this.toString = function(){
+                var stringFormat = "La somme de %s1 de %s5 %s2 %s3. La nouvelle somme de %s1 est %s4.";
+                return Indicator.getLocal(indId)
+                .then(function(indicator){
+                    return stringFormat.replace(/%s1/g, param)
+                    .replace(/%s2/g, comparatorString[comparator.toString().split(' ')[0]])
+                    .replace(/%s3/g, comparator.toString().split(' ')[1])
+                    .replace(/%s4/g, _.sum(indicator.value, param))
+                    .replace(/%s5/g, indicator.label);
+                });
             }
         }
     }
@@ -44,7 +69,7 @@ angular.module('gisMobile').service('Alert',function(Indicator, $q, localStorage
         
         //retrieve rev and id
         var rev = args.pop(), id = args.pop(), resolved = args.pop();
-
+        console.log(ctx);
         //the args become the params
         var params = args;
 
@@ -56,6 +81,7 @@ angular.module('gisMobile').service('Alert',function(Indicator, $q, localStorage
         //Resolve an alert
         this.resolve = function(version){
             ctx.resolved = version;
+            console.log(ctx);
             save(ctx);
         }
 
@@ -71,6 +97,10 @@ angular.module('gisMobile').service('Alert',function(Indicator, $q, localStorage
             }
         }
 
+        this.remove = function(){
+            remove(this.serialize());
+        }
+
         //Extend Alert with param type
         alertType[type].apply(ctx, [comparator].concat(args)); 
     }
@@ -81,6 +111,13 @@ angular.module('gisMobile').service('Alert',function(Indicator, $q, localStorage
         isSmaller : function isSmaller(value, x){ return value < x; },
         isGreater : function isGreater(value, x){ return value > x; },
         isBetween : function isBetween(value, x, y){ return value >= x && value <= y; }
+    };
+
+    var comparatorString = {
+        isEqual : 'est égal à',
+        isGreater : 'est plus grande que',
+        isSmaller : 'est plus petite que',
+        isBetween : 'est entre'
     };
 
     function getComparator(type){
